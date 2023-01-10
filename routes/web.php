e4ab219c733445b094308bdce1f807aef2553487
ThuzarMyint10 <thuzarmyint795@gmail.com>
2023-01-10 13:17:07 +0630

    Laravel 9 Ecommerce - Part 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,17 +32,15 @@ Route::prefix('admin')
             App\Http\Controllers\Admin\DashboardController::class,
             'index',
         ]);
-        // Category Routes
-        Route::get('category', [
-            App\Http\Controllers\Admin\CategoryController::class,
-            'index',
-        ]);
-        Route::get('category/create', [
-            App\Http\Controllers\Admin\CategoryController::class,
-            'create',
-        ]);
-        Route::post('category', [
-            App\Http\Controllers\Admin\CategoryController::class,
-            'store',
-        ]);
+        Route::controller(
+            App\Http\Controllers\Admin\CategoryController::class
+        )->group(function () {
+            Route::get('/category', 'index');
+            Route::get('/category/create', 'create');
+            Route::post('/category', 'store');
+            Route::get('/category/{category}/edit', 'edit');
+            Route::put('/category/{category}', 'update');
+        });
+
+        Route::get('/brands', App\Http\Livewire\Admin\Brand\Index::class);
     });
